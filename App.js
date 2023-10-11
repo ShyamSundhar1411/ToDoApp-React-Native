6cdd4160752c599a0cd6c12335ecdf9cb18515d8
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-10-12 01:06:17 +0530

    chore: added proper folder structure

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar as ExpoStatusBar} from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>ToDo App</Text>
+      <ExpoStatusBar style="auto" />
     </View>
   );
 }
